5a1a177c610e86d3c4c2c46fc0c4eb5b5190a60f
Dimmiditutto <dimmiditutto@gmail.com>
2026-04-15 16:22:03 +0000

    delete

--- frontend/src/services/payments.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './api';
-import { CheckoutRequest, CheckoutResponse } from '../types';
-
-export const paymentService = {
-  async createCheckoutSession(data: CheckoutRequest): Promise<CheckoutResponse> {
-    const response = await api.post<CheckoutResponse>('/payments/create-checkout-session', data);
-    return response.data;
-  },
-};
